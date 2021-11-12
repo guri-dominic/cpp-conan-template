bc1836304202640b753180387725295e39ca71e1
guri-dominic <dominic.guri@gmail.com>
2021-11-11 20:56:10 -0500

    vim ycm and ninja

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 mkdir build && cd build
 conan install ..
-cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=1 # generates compile_commands.json
 
-ln -s compile_commands.json ../compile_commands.json # link compile_commands.json to home dir
+# cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=1 # generates compile_commands.json
+cmake -GNinja ..
+
+# ln -s compile_commands.json ../compile_commands.json # link compile_commands.json to home dir
+ln -s conan_ycm_extra_conf.py ../.ycm_extra_conf.py
+ln -s conan_ycm_flags.json ../.ycm_flags.json
